c364714f880ea3a06c96f9770d3953c6ba2d0438
LazycatLliox <cte-atreyu.blum@sllboces.org>
2025-04-30 17:30:16 +0000

    ahh

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,7 +1,19 @@
+// JavaScript for the hamburger Button
+
 const hamMenu = document.querySelector(".ham-menu");
 const offScreenMenu = document.querySelector(".off-screen-menu");
 
 hamMenu.addEventListener("click",() => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
-})
+});
+
+// Dark mode function
+
+document.getElementsByClassName("DarkMode")
+function DarkMode(){
+    DarkMode.style.backgroundColor = "black";
+    DarkMode.style.Color = "white";
+}
+
+DarkMode();
